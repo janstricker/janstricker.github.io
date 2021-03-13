13af621db2b745d5e255e63e4611df72fc4dcf06
Jan Stricker <kontakt@janstricker.de>
2021-03-14 00:53:29 +0100

    added to-do list

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,41 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-  fontFamily: {
-        sans: ['Fira Sans', 'sans-serif'],
-      },
+    fontFamily: {
+      sans: ['Fira Sans', 'sans-serif'],
+    },
     extend: {
+      minHeight: {
+        xs: "20rem",
+        sm: "24rem",
+        md: "28rem",
+        lg: "32rem",
+        xl: "36rem",
+        "2xl": "42rem",
+        "3xl": "48rem",
+        "4xl": "56rem",
+        "5xl": "64rem",
+        "6xl": "72rem",
+      },
+      maxHeight: {
+        xs: "20rem",
+        sm: "24rem",
+        md: "28rem",
+        lg: "32rem",
+        xl: "36rem",
+        "2xl": "42rem",
+        "3xl": "48rem",
+        "4xl": "56rem",
+        "5xl": "64rem",
+        "6xl": "72rem",
+      },
+      backgroundImage: theme => ({
+        'todo': "url('assets/todo.svg')",
+      })
     },
   },
   variants: {
     extend: {
-      borderRadius: ['hover'],
     },
   },
   plugins: [],
